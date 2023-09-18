6f3a0a1aebb15a0b6d8619da4ed8b92761c20440
Danijel Cigula <danijel.cigula@povio.com>
2023-09-18 08:48:55 +0200

    Added README.md

---
+++ packages/README.md
@@ -0,0 +1,16 @@
+## Description
+
+Example of a Full stack application using NestJS and Svelte with cookies example.
+
+## Installation
+
+```bash
+$ yarn install
+```
+
+## Running the app
+
+```bash
+# development
+$ npx lerna run dev
+```
--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173'],
     credentials: true,
   });
   app.use(cookieParser());
